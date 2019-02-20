dc2534285abc2413a3fe6b4b49591112cd8bf167
Andrey Melnichenko <melnichenko.andrey@gmail.com>
2019-02-20 18:19:01 +0200

    getproperty added

--- fileReader.js
+++ fileReader.js
@@ -1,3 +1,6 @@
 var fs = require('fs');
 var contents = fs.readFileSync('config', 'utf8');
-console.log(contents);
+var strArr = contents.split("\r\n");
+strArr.splice(strArr.length-1,1);
+var myObject = {};
+strArr.map(function (i) {return i.split('=');}).forEach(function (j) {myObject[j[0].trim()] = j[1];});
