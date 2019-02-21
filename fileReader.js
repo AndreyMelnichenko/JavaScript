f1be121e0c06bd33960f01462431712a96f40dd4
Andrey Melnichenko <melnichenko.andrey@gmail.com>
2019-02-21 18:47:59 +0200

    add new test

--- fileReader.js
+++ fileReader.js
@@ -3,4 +3,8 @@ var contents = fs.readFileSync('config', 'utf8');
 var strArr = contents.split("\r\n");
 strArr.splice(strArr.length-1,1);
 var myObject = {};
-strArr.map(function (i) {return i.split('=');}).forEach(function (j) {myObject[j[0].trim()] = j[1];});
+
+module.exports = function(key){
+	strArr.map(function (i) {return i.split('=');}).forEach(function (j) {myObject[j[0].trim()] = j[1];});
+	return myObject[key];
+};
--- test-bpm.js
+++ test-bpm.js
@@ -6,6 +6,7 @@ var wd = require("wd"),
     _ = require('underscore'),
     serverConfigs = require('./helpers/appium-servers');
 var appPath = require('./platform.js');
+var getProperty = require('./fileReader.js');
 
 describe("android webview", function () {
   this.timeout(300000);
@@ -46,26 +47,45 @@ describe("android webview", function () {
 		return driver
 			.sleep(1000)
 			.elementByXPath("//*[@placeholder='mysite.bpmonline.com']")
-			.sendKeys('Andrey')
+			.sendKeys(getProperty("server.adress"))
 			.sleep(1000)
-			.contexts()
-			.then(function (ctxs) {
-				return driver.context(ctxs[ctxs.length - 2]);
-			})
-			.getWindowSize()
-			.then(function (size) {
-				var x = size.width / 2;
-				var y = size.height / 2 + 100;
-				var action = new wd.TouchAction(driver);
-				action.tap({x:x, y:y});
-				return driver.performTouchAction(action);
-			})
-			.contexts()
-			.then(function (ctxs) {
-				return driver.context(ctxs[ctxs.length - 1]);
-			})
+			.elementByCss('#LoginPage')
+			.tap()
+			// .contexts()
+			// .then(function (ctxs) {
+			// 	return driver.context(ctxs[ctxs.length - 2]);
+			// })
+			// .getWindowSize()
+			// .then(function (size) {
+			// 	var x = size.width / 2;
+			// 	var y = size.height / 2 + 100;
+			// 	var action = new wd.TouchAction(driver);
+			// 	action.tap({x:x, y:y});
+			// 	return driver.performTouchAction(action);
+			// })
+			// .contexts()
+			// .then(function (ctxs) {
+			// 	return driver.context(ctxs[ctxs.length - 1]);
+			// })
 			.sleep(1000)
 			.elementByCss("#LoginPage_continueButton > .x-button-label")
+			.tap()
+			.elementByCss(".x-msgbox").should.eventually.exist
+			.elementByCss(".x-msgbox-text > div").text().should.become('Invalid bpm\'online server address specified or the server is not available at the moment')
+			.elementByCss('div[data-item-marker="ok"]').tap()
+			.elementByCss('#LoginPage').should.eventually.exist
+			.sleep(5000);
+	});
+
+	it('DEMO-ENTER', function () {
+		return driver
+			.elementByCss("#LoginPage_demoLoginButton > .x-button-label")
+			.should.eventually.exist
+			.tap()
+			.elementByCss("#DemoLoginPage")
+			.should.eventually.exist
+			.elementByCss("#DemoLoginPage_forwardButton")
+			.should.eventually.exist
 			.tap()
 			.sleep(5000);
 	});
